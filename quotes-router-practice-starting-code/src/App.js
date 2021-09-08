18864f32c836d14db25d14abd0214e7e5d17c027
jagadish123 <mohantyjagadish123@gmail.com>
2021-09-09 00:15:51 +0530

    [quote-router-project] hosting done.

--- quotes-router-practice-starting-code/src/App.js
+++ quotes-router-practice-starting-code/src/App.js
@@ -1,12 +1,19 @@
+import React, { Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 // import { useState } from "react";
 // import NoQuotesFound from "./components/quotes/NoQuotesFound";
-import AllQuotes from "./pages/AllQuotes";
-import QuoteDetails from "./pages/QuoteDetails";
-import NewQuote from "./pages/NewQuote";
+// import AllQuotes from "./pages/AllQuotes";
+// import QuoteDetails from "./pages/QuoteDetails";
+// import NewQuote from "./pages/NewQuote";
 // import MainNavigation from "./components/layout/MainNavigation";
 import Layout from "./components/layout/Layout";
-import NoQuote from "./pages/NoQuote";
+// import NoQuote from "./pages/NoQuote";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote=React.lazy(()=>import("./pages/NewQuote") );
+const AllQuotes=React.lazy(()=>import("./pages/AllQuotes") );
+const QuoteDetails=React.lazy(()=>import("./pages/QuoteDetails") );
+const NoQuote=React.lazy(()=>import("./pages/NoQuote") );
 function App() {
   // const [quotes, setQuotes] = useState([]);
   // const quoteAddHandler = (quote) => {
@@ -17,22 +24,25 @@ function App() {
   // };
   return (
     <Layout>
-
+      <Suspense fallback={<div className="centered">
+        <LoadingSpinner/>
+      </div>}>
       <Switch>
       <Route path="/"  exact>
         <Redirect to='/quotelist'/>
       </Route>
-        <Route path="/newquote">
-          <NewQuote/>
-        </Route>
         <Route path="/quotelist" exact>
           <AllQuotes/>
         </Route>
         <Route path="/quotelist/:quoteID"  component={QuoteDetails}/>
+        <Route path="/newquote">
+          <NewQuote/>
+        </Route>
         <Route path="*">
           <NoQuote/>
         </Route>
       </Switch>
+      </Suspense>
     </Layout>
     
   );
